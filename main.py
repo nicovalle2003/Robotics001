e9d470aa42f83d95528e97eef6085951945e4e06
nicovalle2003 <nicolas.vallesp.14@gmail.com>
2025-02-26 04:07:55 -1200

    Codigo DH ABB

--- main.py
+++
@@ -1,5 +0,0 @@
-import math
-radio = float(input("Introduce el radio de la circunferencia: "))
-perimetro = 2 * math.pi * radio
-area = math.pi * radio ** 2
-print(f"El perímetro de la circunferencia es {perimetro:.2f} y el área es {area:.2f}")
