0482ac6caa04ee6aaa2fc786bee933e24853d82d
nicovalle2003 <nicolas.vallesp.14@gmail.com>
2025-03-01 16:32:21 -0600

    update

--- Fanuc CR-35ia.py
+++ Fanuc CR-35ia.py
@@ -16,23 +16,24 @@ joint6 = np.deg2rad(0)
 
 print("Denavit-Hartenberg Parameters WITH TOOLBOX: ")
 
-d1, a2, d4, d6 = 0.185, 0.410, 0.430, 0.145
-alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = np.pi/-2, 0, np.pi/2, np.pi/2, np.pi/-2, 0
+d1,d4,d6 = 1.180, 0.86, 0.100
+a1, a2, a3 = 0.150, 0.79, 0.150
+alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = np.pi/2, 0, np.pi/2, np.pi/-2, np.pi/2, 0
 
 robot = rtb.DHRobot(
     [
-        rtb.RevoluteDH(d=float(d1), a=0, alpha=float(alpha1), offset=0, qlim=[-0,0]),
-        rtb.RevoluteDH(d=0, a=float(a2), alpha=float(alpha2), offset=float(np.pi/-2), qlim=[-0,0]),
-        rtb.RevoluteDH(d=0, a=0, alpha=float(alpha3), offset=0, qlim=[-0,0]),
-        rtb.RevoluteDH(d=float(d4), a=0, alpha=float(alpha4), offset=0,qlim=[-0,0]),
-        rtb.RevoluteDH(d=0, a=0, alpha=float(alpha5), offset=0, qlim=[-0,0]),
-        rtb.RevoluteDH(d=float(d6), a=0, alpha=float(alpha6), offset=0, qlim=[-0,0])
+        rtb.RevoluteDH(d=float(d1), a=float(a1), alpha=float(alpha1), offset=0, qlim=[-2.96,2.96]),
+        rtb.RevoluteDH(d=0, a=float(a2), alpha=float(alpha2), offset=float(np.pi/2), qlim=[-1.43,1.43]),
+        rtb.RevoluteDH(d=0, a=float(a3), alpha=float(alpha3), offset=0, qlim=[-2.25,2.25]),
+        rtb.RevoluteDH(d=float(d4), a=0, alpha=float(alpha4), offset=0,qlim=[-3.49,3.49]),
+        rtb.RevoluteDH(d=0, a=0, alpha=float(alpha5), offset=0, qlim=[-1.91,1.91]),
+        rtb.RevoluteDH(d=float(d6), a=0, alpha=float(alpha6), offset=0, qlim=[-7.85,7.85])
     ], name = "Fanuc CR-35ia", base = SE3(0, 0, 0)
 )
 
 print(robot)
 q =np.array([joint1, joint2, joint3, joint4, joint5, joint6])
-robot.plot(q=q, backend = 'pyplot', dt = 10, limits = [-0.8,0.8,-0.8,0.8,-0.4,0.6,], shadow = True, jointaxes = True)
+robot.plot(q=q, backend = 'pyplot', dt = 500, limits = [-1.5,1.5,-1.5,1.5,-0.8,0.12,], shadow = True, jointaxes = True)
 plt.show()
 #q1 = np.array([0, np.pi/2, 0, 0, 0, 0])
 print(robot.fkine(q))
