712030a5200aa811a486a01a4c64441de204caa5
nicovalle2003 <nicolas.vallesp.14@gmail.com>
2025-03-07 00:06:22 -0600

    Update Inv Espacio Cartesiano ABB IRB 120-3.py

--- Inv Espacio Cartesiano ABB IRB 120-3.py
+++ Inv Espacio Cartesiano ABB IRB 120-3.py
@@ -24,19 +24,6 @@ print(robot)
 q1 = np.array([0, 0, 0, 0, 0, 0])
 robot.teach(q1)
 print(robot.fkine(q1))
-# joint1 = 0  # In degrees
-# joint2 = 0
-# joint3 = 0
-# joint4 = 0
-# joint5 = 0
-# joint6 = 0
-
-# T = robot.fkine([np.deg2rad(joint1), np.deg2rad(joint2), np.deg2rad(joint3), 
-#                  np.deg2rad(joint4), np.deg2rad(joint5), np.deg2rad(joint6)])
-# Tn = np.array(T).astype(np.float64)
-# x, y, z = Tn[0, 3], Tn[1, 3], Tn[2, 3]
-# print(f"Los ángulos en la Cdirecta son ({joint1 :.2f}, {joint2 :.2f}, {joint3 :.2f}, {joint4 :.2f}, {joint5 :.2f}, {joint6 :.2f}) que pasamos a la inversa...")
-# print(f"La coordenada es ({x :.3f}, {y :.3f}, {z :.3f}) que pasamos a la inversa...")
 
 robot.tool = SE3.OA([1, 0, 0], [0, -1, 0])
 robot.configurations_str('ru')  # right, elbow up
@@ -87,16 +74,6 @@ T_Tool = SE3(-0.15, 0, 0) * SE3.Trans(xyz_traj) #SE3.OA([0, -1, 0], [1, 0, 0])
 sol = robot.ikine_LM(T_Tool, q0=robot.qz, mask=[1, 1, 1, 0, 0, 0])
 print('Método 2, usando mstraj')
 print(sol.success)
-#robot.plot(q=sol.q, backend='pyplot', block=True, limits=[-0.8, 0.8, -0.8, 0.8, -0.1, 1], eeframe=True, shadow=True, jointaxes=True)
-#robot.plot(q=sol2.q, backend='pyplot', block=True, limits=[-0.8, 0.8, -0.8, 0.8, -0.1, 1], eeframe=True, shadow=True, jointaxes=True)
-
-# if not sol2.success:
-#     print("Inverse kinematics failed for sol2.")
-#     print("Target pose:", SE3(T))
-#     print("Initial guess:", robot.qz)
-#     print("Solution:", sol2.q)
-#     print("Residual:", sol2.residual)
-#     print("Reason:", sol2.reason)
 
 p_lim=[-1, 1, -1, 1, -0.15, 1.5]
 plot_robot_trajectory(
